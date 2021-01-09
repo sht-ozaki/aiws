89b80039c44faeb8665c334aaa86072b7e2ecf4d
ryan <dooioomoo@gmail.com>
2021-01-09 16:01:25 +0900

    .

--- aiws.bat
+++ aiws.bat
@@ -7,6 +7,19 @@ SET WSL_FILE=baota
 SET DOCKERBOTA=https://github.com/dooioomoo/docker-bota.git
 
 
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+cd /d "%~dp0"
+echo 当前运行路径是：%CD%
+echo 已获取管理员权限
+echo .
+echo .
+echo .
 call :Resume
 goto %current%
 goto :eof
