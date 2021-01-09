5133bd5940cf20889921cd3f47bfdebab7cc47ee
ryan <dooioomoo@gmail.com>
2021-01-09 15:53:12 +0900

    .

--- aiws.bat
+++ aiws.bat
@@ -12,7 +12,7 @@ goto %current%
 goto :eof
 
 setlocal
-:step1
+:one
 echo 现在开始准备安装系统环境
 echo .
 echo 主要用于基于Centos和docker的前端开发和后台开发环境
@@ -92,7 +92,7 @@ powershell Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-
 
 ::Add script to Run key
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v %~n0 /d %~dpnx0 /f
-echo step2 >%~dp0current.txt
+echo two >%~dp0current.txt
 echo -- Section one --
 pause
 shutdown -r -t 0
@@ -100,7 +100,7 @@ shutdown -r -t 0
 goto :eof
 
 
-:setp2
+:two
 ::Remove script from Run key
 reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v %~n0 /f
 del %~dp0current.txt
@@ -149,6 +149,6 @@ goto :eof
 if exist %~dp0current.txt (
     set /p current=<%~dp0current.txt
 ) else (
-    set current=step1
+    set current=one
 )
 endlocal
