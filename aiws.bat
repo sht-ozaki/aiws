72040b76ad2e46dea205d2a34743d053b44fe62b
dooioomoo <dooioomoo@gmail.com>
2021-01-12 14:58:32 +0900

    Update aiws.bat

--- aiws.bat
+++ aiws.bat
@@ -44,7 +44,7 @@ echo.
 echo.
 echo.
 echo 正在安装 choco ...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n=allowGlobalConfirmation
 echo.
 echo.
